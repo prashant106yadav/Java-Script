8f42c03e3d571ee78e9b80b0ec21312a5b0831df
prashant106yadav <prashant106yadav@gmail.com>
2026-01-20 23:46:26 +0530

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+let name = "prashant"
+let age = 20
+let isloggedin = false
+let state;
+
+// number => 2 to the power of 53(range)
+// bigint :- used for larger numbers 
+// string =>"" :- used for text
+// boolean => true/false 
+// null => standalone value
+// undefined =>
+// symbol => unique :- to define uniqueness of an object
+
+// object :- to be studied later
+
+// typeof :- used to know the dataTypes
+
+console.log(typeof null)  //   object
+console.log(typeof undefined)   // undefined
+console.log([name, age, isloggedin,state])
