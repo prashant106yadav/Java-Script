37f54e7d21ef16ac7c85a8c159f42e8c4dca31b0
prashant106yadav <prashant106yadav@gmail.com>
2026-01-20 20:24:01 +0530

    learned about variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345;
+let accountEmail = "yadav123@gmail.com";
+var accountPasssword = "1234";
+accountCity = "Ghaziabad";
+let accountState;
+
+// accountId = 54321;   not allowed because redeclaration of const is not alloewed
+
+accountEmail = "itachi@gmail.com";
+accountPassword = "1718"
+accountCity = "Delhi"
+
+/*
+prefer not to use var because of block and functioal scope.
+*/
+
+console.log([accountId, accountPassword, accountEmail, accountCity, accountState]);
