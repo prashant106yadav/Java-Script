d2d31c0652d53cc44d876223779347c12b6c0379
prashant106yadav <prashant106yadav@gmail.com>
2026-01-22 00:31:21 +0530

    conversion of dataTypes

---
+++ 01_basics/conversionOfDataTypes.js
@@ -0,0 +1,38 @@
+// let score = 69                  { value in number }
+// let score = "69"               //  { value in string }
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)         // conversion of string to number
+// console.log(typeof valueInNumber)   
+
+// let score2 = "17abc"
+// let valueInNumber2 = Number(score2)      // conversion
+
+// console.log(typeof valueInNumber2);      // datatypes converted from string to number 
+// console.log(valueInNumber2);             // but value of number is NaN(not a number)
+
+
+
+// let score3 = null
+// let score4 = undefined 
+
+// let valueInNumber3 = Number(score3)
+// let valueInNumber4 = Number(score4)
+// console.log([typeof score3, typeof score4]); 
+// console.log([score3, score4]);
+
+
+
+// let isLoggedIn = 1                   // value in number
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)        // conversion of number to boolean
+// console.log(booleanIsLoggedIn)
+
+
+let alpha = 18                              // value in number 
+let stringAlpha = String(alpha)             // conversion of number to string 
+
+console.log(stringAlpha);                   // output value displayed wiil be number         
+console.log(typeof stringAlpha);            // but datatype will be string
+
+
